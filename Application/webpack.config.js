49afc7fb7eb7c0840ca384390dbeabcde494144c
Starvon Washington <staronejazz@yahoo.com>
2018-03-26 22:56:09 -0700

    webpack build

---
+++ Application/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["./src/js/app.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
