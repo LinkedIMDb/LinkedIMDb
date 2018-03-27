fbd60f0a3e12566042d854f3968d7b5476aeeb38
Starvon Washington <staronejazz@yahoo.com>
2018-03-27 13:39:57 -0700

    react webpack babel

---
+++ Application/client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'LinkedImdb';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+// module.hot.accept();
