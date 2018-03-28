cfbe80d1ea3521d73208dd9ad213244709a79f35
Annie <annie.difiore@gmail.com>
2018-03-27 19:57:41 -0700

    serve static html and consolidate package/webpack

--- Application/client/src/components/App.jsx
+++ Application/client/src/components/App.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { Link, IndexLink } from 'react-router';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import LoginForm from './LoginForm';
-import SignUpForm from './SignUpForm';
-import Testing from './Testing';
+import LoginForm from './LoginForm.jsx';
+import SignUpForm from './SignUpForm.jsx';
+import Testing from './Testing.jsx';
 
 
 const App = () => (
