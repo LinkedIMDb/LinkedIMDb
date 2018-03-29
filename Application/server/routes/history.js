ad874e99917153df9149372c1e05e497d8c17a8c
Annie <annie.difiore@gmail.com>
2018-03-29 13:52:55 -0700

    add history controller

--- Application/server/routes/history.js
+++ Application/server/routes/history.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = new express.Router();
+const historyController = require('../controllers/historyController');
+
+router.get('/getHistory', );
+
 
 
 module.exports = router;
