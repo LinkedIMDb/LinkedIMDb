22089914e841ff0e47da2e7e6b44afa3a2b28c39
Max White <maxwell.oliver.white@gmail.com>
2018-03-29 13:57:24 -0700

    add path route

--- Application/server/routes/history.js
+++ Application/server/routes/history.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = new express.Router();
 
+router.post('/save',
+  (req, res) => {
+    res.status(200).json({});
+  }
+);
 
 module.exports = router;
